1b6e2fc6ca67bc20fc9c6755f7d5a6108d781211
joshuadias06 <joshuadiasbf@gmail.com>
2025-04-30 12:28:14 -0300

    Update WelcomeActivity

--- app/src/main/java/com/primus/nexus/welcome/ActivityWelcome.kt
+++ app/src/main/java/com/primus/nexus/welcome/ActivityWelcome.kt
@@ -3,6 +3,8 @@ package com.primus.nexus.welcome
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,6 +24,12 @@ class ActivityWelcome : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Define progressão inicial como zero
+        val progressBar = findViewById<ProgressBar>(R.id.progressBarXP)
+        val tvNivel = findViewById<TextView>(R.id.tvNivel)
+        progressBar.progress = 0
+        tvNivel.text = "Nv. 0"
 
         // Ação do botão "Começar"
         val btnComecar = findViewById<Button>(R.id.btnComecar)
